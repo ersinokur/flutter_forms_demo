3b175dc7ab4c5bd1a60e0089524d6a309e21bad3
Ersin <ersin.okur@gmail.com>
2019-01-17 13:10:26 +0300

    save operatin added with forms

--- lib/screens/customerAdd.dart
+++ lib/screens/customerAdd.dart
@@ -1,11 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_forms_demo/mixins/validation_mixin.dart';
+import 'package:flutter_forms_demo/models/customer.dart';
 
 class CustomerAdd extends StatefulWidget {
   _CustomerAddState createState() => _CustomerAddState();
 }
 
 class _CustomerAddState extends State<CustomerAdd> with ValidationMixin {
+  final customer = new Customer();
   final formKey = GlobalKey<FormState>();
   @override
   Widget build(BuildContext context) {
@@ -28,6 +30,9 @@ class _CustomerAddState extends State<CustomerAdd> with ValidationMixin {
 
   Widget firstNameField() {
     return TextFormField(
+        onSaved: (String value) {
+          customer.firstName = value;
+        },
         validator: validateFirstName,
         decoration: InputDecoration(
             labelText: "First Name", hintText: "type your name"));
@@ -35,6 +40,9 @@ class _CustomerAddState extends State<CustomerAdd> with ValidationMixin {
 
   Widget lastNameField() {
     return TextFormField(
+        onSaved: (String value) {
+          customer.lastName = value;
+        },
         validator: validateLastName,
         decoration: InputDecoration(
             labelText: "Last Name", hintText: "type your last name"));
@@ -42,6 +50,9 @@ class _CustomerAddState extends State<CustomerAdd> with ValidationMixin {
 
   Widget emailNameField() {
     return TextFormField(
+        onSaved: (String value) {
+          customer.emailName = value;
+        },
         validator: validateEmail,
         keyboardType: TextInputType.emailAddress,
         decoration: InputDecoration(
@@ -50,6 +61,9 @@ class _CustomerAddState extends State<CustomerAdd> with ValidationMixin {
 
   Widget passwordField() {
     return TextFormField(
+        onSaved: (String value) {
+          customer.passwordName = value;
+        },
         obscureText: true,
         decoration: InputDecoration(
             labelText: "password", hintText: "type your password"));
@@ -59,7 +73,9 @@ class _CustomerAddState extends State<CustomerAdd> with ValidationMixin {
     return RaisedButton(
       child: Text("Save"),
       onPressed: () {
-        this.formKey.currentState.validate();
+        if (this.formKey.currentState.validate()) {
+          formKey.currentState.save();
+        }
       },
     );
   }
